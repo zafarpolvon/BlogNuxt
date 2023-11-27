601ff8079b97f41bca12bae5aa0f98787aa22392
Zafar <su10409@studenti.polito.it>
2023-11-27 07:05:55 +0500

    refactoring deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,11 +13,7 @@ export default defineNuxtConfig({
           path: '/post/:id',
           file: resolve(__dirname, './pages/post/Index.vue'),
         });
-        pages.push({
-          name: 'comments',
-          path: '/comments',
-          file: resolve(__dirname, './pages/comments/Index.vue'),
-        });
+       
     }
   },
  
