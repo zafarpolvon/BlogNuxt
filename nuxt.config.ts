fb7a90722b6a1edfd18e4c4ff93d83409f335fd8
Zafar <su10409@studenti.polito.it>
2023-11-27 06:11:23 +0500

    refactoring deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,26 @@
-import { resolve } from "pathe";
+import { defineNuxtConfig } from 'nuxt3';
+import { resolve } from "path";
+
 export default defineNuxtConfig({
   hooks: {
     'pages:extend': async (pages) => {
-        pages.push({
-            name: 'index',
-            path: '/',
-            file: resolve(__dirname, './pages/Index.vue'),
-          }
-        );
-        pages.push({
-          name: 'post',
-          path: '/post/:id',
-          file: resolve(__dirname, './pages/post/Index.vue'),
-        });
-        pages.push({
-          name: 'comments',
-          path: '/comments',
-          file: resolve(__dirname, './pages/comments/Index.vue'),
-        });
+      pages.push({
+        name: 'index',
+        path: '/',
+        file: resolve(__dirname, './pages/Index.vue'),
+      });
+      pages.push({
+        name: 'post',
+        path: '/post/:id',
+        file: resolve(__dirname, './pages/post/Index.vue'),
+      });
+      pages.push({
+        name: 'comments',
+        path: '/comments',
+        file: resolve(__dirname, './pages/comments/Index.vue'),
+      });
     }
   },
- 
-
 
   runtimeConfig: {
     apiSecret: '123',
@@ -29,12 +28,10 @@ export default defineNuxtConfig({
       apiBase: '/api'
     }
   },
+
   build: {
     rollupOptions: {
       external: ['/home/runner/work/BlogNuxt/BlogNuxt/pages/comments/Index.vue?macro=true']
     }
   }
-  
-  
- 
-})
+});
